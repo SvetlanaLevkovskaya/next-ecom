78da261c2773a0aecf17fa634d5262297733eb6b
svetlana <levkovskayase@gmail.com>
2024-10-30 13:04:16 +0300

    feat: improve styles at ProductDetails

--- src/app/product/[id]/_ui/ProductDetails/ProductDetails.tsx
+++ src/app/product/[id]/_ui/ProductDetails/ProductDetails.tsx
@@ -20,13 +20,15 @@ export const ProductDetails = ({ product }: { product: Products }) => {
   return (
     <section className="flex flex-col md:flex-row justify-center items-center mx-auto gap-6 my-10">
       <div className="flex flex-col md:flex-row items-center justify-center gap-8">
-        <ImageWithFallback
-          src={product.image}
-          alt={product.title}
-          width={256}
-          height={256}
-          className="object-cover"
-        />
+        <div className="w-[206px] h-[206px] flex justify-center items-center  flex-shrink-0">
+          <ImageWithFallback
+            src={product.image}
+            alt={product.title}
+            width={226}
+            height={226}
+            className="object-cover w-auto h-auto"
+          />
+        </div>
 
         <div className="flex flex-col gap-4 w-full md:w-auto divide-y ">
           <div>
@@ -57,12 +59,12 @@ export const ProductDetails = ({ product }: { product: Products }) => {
           </div>
 
           <div>
-            <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4 mt-10">
-              <div className="w-full md:w-3/4">
+            <div className="flex flex-col md:flex-row items-start md:items-center gap-6 mt-10">
+              <div className="w-full md:w-3/4 flex-grow">
                 <h3 className="text-sm font-bold mb-8">Description</h3>
                 <p className="text-sm text-gray-700">{product.description}</p>
               </div>
-              <div className="flex flex-col items-center md:items-end gap-2">
+              <div className="flex flex-col items-center md:items-end gap-2 self-start ">
                 <p className="text-2xl font-bold text-gray-900">{product.price} $</p>
                 <button className="px-8 py-2 bg-amber-400 text-white font-medium rounded-md hover:bg-amber-500 transition-all2">
                   Buy
